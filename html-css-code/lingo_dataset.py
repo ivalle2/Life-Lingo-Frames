5e8dbf4052e58e064e7fa673ab3732615d726b3a
NavneethKunkatla <119468517+NavneethKunkatla@users.noreply.github.com>
2024-04-27 16:00:15 -0700

    Update lingo_dataset.py

--- html-css-code/lingo_dataset.py
+++ html-css-code/lingo_dataset.py
@@ -14,32 +14,34 @@ os.environ['KAGGLE_KEY'] = 'aa629e0ddace435ec07eb2d585fa5006'
 def main():
     
     # Scrapping the net to find a dataset
-    def makeDataFrame():
         
-        api = KaggleApi()
-        api.authenticate()
+    api = KaggleApi()
+    api.authenticate()
 
-        dataset_path = 'lonnieqin/englishspanish-translation-dataset'
-        file_name = 'data.csv'
+    dataset_path = 'lonnieqin/englishspanish-translation-dataset'
+    file_name = 'data.csv'
         
-        api.dataset_download_file(dataset_path, file_name, path='./')
+    api.dataset_download_file(dataset_path, file_name, path='./')
 
-        # Accessing the downloaded dataset from repository
-        csv_file = "C:/Users/navak/Documents/GitHub/Life-Lingo-Frames/html-css-code/data.csv"
+    # Accessing the downloaded dataset from repository
+    csv_file = "C:/Users/navak/Documents/GitHub/Life-Lingo-Frames/html-css-code/data.csv"
         
-        df = pd.read_csv(csv_file)
-        # Check if the file exists
-        if os.path.exists(csv_file):
-            # Return the CSV file
-            return df
-        else:
-            print(f"File not found: {csv_file}")
+    df = pd.read_csv(csv_file)
+    # Check if the file exists
+    if os.path.exists(csv_file):
+        # Return the CSV file
+        return df
+    else:
+        print(f"File not found: {csv_file}")
     
-    def create_translation_dict(df):
+    # Create translation dictionary 
+
     # Drop any duplicates and reset the index
     df_unique = df.drop_duplicates().reset_index(drop=True)
+
     # Create a dictionary with English as keys and Spanish as values
     translation_dict = pd.Series(df_unique['spanish'].values, index=df_unique['english']).to_dict()
-    
-    return 
+
+    json_data = json.dumps(words)
+    return json_data
 main()
